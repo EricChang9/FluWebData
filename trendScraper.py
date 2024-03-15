eb07fad238a7e3fa20ddb6897049b6b912596738
Eric Chang <eric04.chang@gmail.com>
2024-03-15 13:43:55 -0400

    created basic script but unable to test yet

--- trendScraper.py
+++ trendScraper.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+from pytrends.request import TrendReq
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 
@@ -8,4 +11,9 @@ service = Service(executable_path=PATH)
 driver = webdriver.Chrome(service=service)
 
 driver.get("https://trends.google.com/trends/explore?q=flu&date=now%201-d&geo=US&hl=en")
+
+elements = driver.find_elements(By.TAG_NAME, "span" )
+print(elements)
+
 driver.close()
+
