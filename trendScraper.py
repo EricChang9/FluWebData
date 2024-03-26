6153f9cc875077b0d4ffabb3441888a2fcf0d7e0
Eric Chang <eric04.chang@gmail.com>
2024-03-26 01:01:44 -0400

    save file

--- trendScraper.py
+++ trendScraper.py
@@ -3,6 +3,18 @@ import time
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
+#this is the node class that we will create the tree from 
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+
+    def add_child(self, obj):
+        self.children.append(obj)
+
+        
+
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 
 service = Service(executable_path=PATH)
@@ -12,7 +24,9 @@ driver.get("https://trends.google.com")
 
 element = driver.find_element(By.XPATH, "//input[@id = 'i7']" )
 element.send_keys("flu", Keys.ENTER)
-print(element)
+
 time.sleep(10)
 driver.close()
+
+
 
